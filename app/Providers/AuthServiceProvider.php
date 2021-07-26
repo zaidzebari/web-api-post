492d26310f6ce563d84f3f1dd4c040c68ef340de
Zaid Salah <zaidsalah351@gmail.com>
2021-07-26 15:24:09 +0300

    updated auth provider policy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -13,7 +13,8 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-         'App\Models\User' => 'App\Policies\TopicPolicy',
+        'App\Models\Topic' => 'App\Policies\TopicPolicy',
+        'App\Models\Post' => 'App\Policies\TopicPolicy',
     ];
 
     /**
