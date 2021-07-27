1918aaa9f320fbb1a7fadb1271371676b90ec443
Zaid Salah <zaidsalah351@gmail.com>
2021-07-27 09:28:49 +0300

    added like table

--- database/migrations/2021_07_27_062439_create_likes_table.php
+++ database/migrations/2021_07_27_062439_create_likes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostsTable extends Migration
+class CreateLikesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,14 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('likes', function (Blueprint $table) {
             $table->id();
-            $table->string('body');
-
+            $table->unsignedBigInteger('likeable_id');
+            $table->string('likeable_type');
             $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('topic_id');
-
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('topic_id')->references('id')->on('topics')->onDelete('cascade');
             $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
         });
     }
 
@@ -33,6 +31,6 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('likes');
     }
 }
